4bae2be734d008ff783fd9762b813e2a1478a6ad
Ronan <galv.ronan@gmail.com>
2021-04-19 17:11:47 -0400

    Inquirer questions for multiple projects.

--- app.js
+++ app.js
@@ -36,13 +36,84 @@ const inquirer = require('inquirer');
 //     // insert fs.open?
 // });
 
-// array of object known as question object 
-inquirer
-    .prompt([
+// Function that returns result of inquirer, which is a Promise
+const promptUser = () => {
+    // array of object known as question object
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'Enter your GitHub Username'
+            },
+            {
+                type: 'input',
+                name: 'about',
+                message: 'Provide some information about yourself:'
+            }
+        ]);
+};
+
+const promptProject = portfolioData => {
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = []
+    }
+
+    console.log(`
+=================
+Add a New Project
+=================
+`);
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
         }
     ])
-    .then(answers => console.log(answers));
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+
+        if(projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData
+        }
+    });
+}
+
+// then is appended to function call since it returns a Promise
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData)
+    })
